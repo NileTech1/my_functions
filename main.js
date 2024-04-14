fa376e0daffc7d995a3fe3b8be76c07f0c79607e
naaa <na0908nna@gmail.com>
2024-04-14 14:04:09 +0300

    ---

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+import { Client, Databases } from "node-appwrite"
+
+const PROJECT_ID = process.env.PROJECT_ID
+const DB_ID = process.env.DB_ID
+const COLLECTION_ID_PROFILES = process.env.COLLECTION_ID_PROFILES
+
 export default async ({ req, res, log, error }) => {
-    return res.send("Hello world!")
+
+
+    const client = new Client()
+    client
+        .setEndpoint('https://cloud.appwrite.io/v1')
+        .setProject(PROJECT_ID)
+
+    if (req.method == 'GET') {
+        const response = await db.listDocuments(
+            DB_ID,
+            COLLECTION_ID_PROFILES
+        )
+        return res.json(response.documents)
+    }
+
+    return res.send("Hello Nile !")
 }
